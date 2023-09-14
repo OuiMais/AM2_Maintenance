669eb2e8bc1ab76c51ebc82d96711b34c7734820
OuiMais <flohofbauer@icloud.com>
2023-09-14 12:52:39 +0200

    Update by PyCharm

---
+++ deleteAllPlanning.py
@@ -0,0 +1,82 @@
+"""
+    Projet : AM2_Auto / deleteAllPlanning
+    Date Creation : 07/12/2022
+    Date Revision : 28/08/2023
+    Entreprise : 3SC4P3
+    Auteur: Florian HOFBAUER
+    Contact :
+    But : Permet de vider tous les plannings
+"""
+
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+import time
+
+
+# Print iterations progress
+def printProgressBar(iteration, total, prefix='', suffix='', decimals=1, length=100, fill='█', printEnd=""):
+    """
+    Call in a loop to create terminal progress bar
+    @params:
+        iteration   - Required  : current iteration (Int)
+        total       - Required  : total iterations (Int)
+        prefix      - Optional  : prefix string (Str)
+        suffix      - Optional  : suffix string (Str)
+        decimals    - Optional  : positive number of decimals in percent complete (Int)
+        length      - Optional  : character length of bar (Int)
+        fill        - Optional  : bar fill character (Str)
+        printEnd    - Optional  : end character (e.g. "\r", "\r\n") (Str)
+    """
+    percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
+    filledLength = int(length * iteration // total)
+    bar = fill * filledLength + '-' * (length - filledLength)
+    print(f'\r{prefix} |{bar}| {percent}% {suffix}', end=printEnd)
+    # Print New Line on Complete
+    if iteration == total:
+        print()
+
+
+# Option for website (no screen open)
+options = Options()
+# options.add_argument('--headless')
+
+# Initiate the browser
+browser = webdriver.Chrome(options=options)
+
+# Open the Website
+browser.get('https://www.airlines-manager.com/network/generalplanning')
+
+# Your  credentials
+name = 'flohofbauer@icloud.com'
+mdp = 'Flo17Titi!'
+
+# name = 'floriangaming55@gmail.com'
+# mdp = 'floflo55'
+
+# Fill credentials
+browser.find_element(by=By.NAME, value='_username').send_keys(name)
+browser.find_element(by=By.NAME, value='_password').send_keys(mdp)
+
+# Click Log In
+browser.find_element(by=By.ID, value='loginSubmit').click()
+time.sleep(5)
+
+#Click accept cookies
+browser.find_element(by=By.CLASS_NAME, value='cc-compliance').click()
+time.sleep(5)
+
+planning = browser.find_element(By.ID, "planning")
+links = planning.find_elements(By.TAG_NAME, "a")
+
+linked = [elmt.get_attribute("href") for elmt in links]
+
+for link in linked:
+    browser.get(link)
+    time.sleep(2)
+    browser.find_element(by=By.CLASS_NAME, value='validBtn').click()
+    time.sleep(2)
+    browser.find_element(by=By.CLASS_NAME, value='planningBtnCenter').click()
+    time.sleep(2)
+
+browser.close()
